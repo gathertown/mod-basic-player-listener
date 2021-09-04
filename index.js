64f3474c336d171bbd2c268816280433e204f3bd
Nate Foss <npfoss@gmail.com>
2021-09-04 17:15:49 -0400

    example listening to moves

---
+++ index.js
@@ -0,0 +1,21 @@
+const { API_KEY } = require("./api-key");
+const { Game } = require("@gathertown/gather-game-client");
+global.WebSocket = require("isomorphic-ws");
+
+/**** setup ****/
+
+// what's going on here is better explained in the docs:
+// https://suave-trip-b63.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
+const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
+game.connect("oxrhEtb3sV7VutbQ\\GatherOffice"); // replace with your spaceId of choice
+game.subscribeToConnection((connected) => console.log("connected?", connected));
+
+/**** the good stuff ****/
+
+game.subscribeToEvent("playerMoves", (data, context) => {
+  console.log(
+    context?.player?.name ?? context.playerId,
+    "moved in direction",
+    data.playerMoves.direction
+  );
+});
