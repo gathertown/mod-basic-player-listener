876ffbdeece71832c384b8aa48bfd6d615aa84a2
Nate Foss <npfoss@gmail.com>
2021-09-07 12:03:15 -0400

    update docs link

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 basic example of subscribing to player movement in a space
 
-check out the [websocket API docs](https://suave-trip-b63.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063) for more!
+check out the [websocket API docs](https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063) for more!
 
 ## setup
 
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ global.WebSocket = require("isomorphic-ws");
 /**** setup ****/
 
 // what's going on here is better explained in the docs:
-// https://suave-trip-b63.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
+// https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
 const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
 game.connect("oxrhEtb3sV7VutbQ\\GatherOffice"); // replace with your spaceId of choice
 game.subscribeToConnection((connected) => console.log("connected?", connected));
