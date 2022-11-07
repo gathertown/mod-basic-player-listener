d30b8e551caf18b40ffec826b4c060b0d7ad5a61
Nate Foss <npfoss@gmail.com>
2022-11-07 15:57:31 -0800

    upgrade to v38.0.1

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ global.WebSocket = require("isomorphic-ws");
 
 // what's going on here is better explained in the docs:
 // https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
-const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
-game.connect("oxrhEtb3sV7VutbQ\\GatherOffice"); // replace with your spaceId of choice
+const game = new Game("oxrhEtb3sV7VutbQ\\GatherOffice", () => Promise.resolve({ apiKey: API_KEY }));
+// replace with your spaceId of choice ^^^^^^^^^^^
+game.connect();
 game.subscribeToConnection((connected) => console.log("connected?", connected));
 
 /**** the good stuff ****/
